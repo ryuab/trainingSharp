13885df84ef1119ba5a9524766b754e59f2d6d09
ryuab <r@r.r>
2022-04-26 02:04:52 +0000

    asda

---
+++ desafio1.cs
@@ -0,0 +1,20 @@
+using System;
+namespace dotnetcore
+{
+    class Program
+   {
+        static void main(string[] args){
+            int conta,taxa,resultado = 0   //declarando e inicializando
+            
+            Console.Write("Digite o valor da conta");
+            conta = Console.ReadLine(); //atribuindo a conta
+            
+            Console.Write("Digite o valor da taxa de garcom");
+            taxa = Console.ReadLine(); //atribuindo a taxa entrada do usario
+            
+            resultado =  conta + ((conta * taxa )/100);
+            
+            Console.Write($ "Sua conta: R{conta:c} + {taxa:%} da o valor total de R{resultado:c}");
+        }
+   }
+}
